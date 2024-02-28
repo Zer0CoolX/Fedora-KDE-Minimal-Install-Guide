39d7db373c215cd4f5c4b4bb28e836c30c5a942b
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2024-02-28 01:17:02 +0100

    updated package list, added baloo disabling and graphical login activation

--- fedora-kde-min-packages.sh
+++ fedora-kde-min-packages.sh
@@ -1,74 +1,73 @@
 #!/bin/env bash
-##### CHECK FOR SUDO or ROOT ##################################
-if ! [ $(id -u) = 0 ]; then
-  echo "This script must be run as sudo or root, try again..."
-  exit 1
-fi
 
-# Install KDE Packages
-dnf install \
-  @"base-x" \
-  @"Common NetworkManager Submodules" \
-  @"Fonts" \
-  @"Hardware Support" \
-  bluedevil \
-  breeze-gtk \
-  breeze-icon-theme \
-  cagibi \
-  colord-kde \
-  cups-pk-helper \
-  dolphin \
-  glibc-all-langpacks \
-  gnome-keyring-pam \
-  kcm_systemd \
-  kde-gtk-config \
-  kde-partitionmanager \
-  kde-print-manager \
-  kde-settings-pulseaudio \
-  kde-style-breeze \
-  kdegraphics-thumbnailers \
-  kdeplasma-addons \
-  kdialog \
-  kdnssd \
-  kf5-akonadi-server \
-  kf5-akonadi-server-mysql \
-  kf5-baloo-file \
-  kf5-kipi-plugins \
-  khotkeys \
-  kmenuedit \
-  konsole5 \
-  kscreen \
-  kscreenlocker \
-  ksshaskpass \
-  ksysguard \
-  kwalletmanager5 \
-  kwebkitpart \
-  kwin \
-  NetworkManager-config-connectivity-fedora \
-  pam-kwallet \
-  phonon-qt5-backend-gstreamer \
-  pinentry-qt \
-  plasma-breeze \
-  plasma-desktop \
-  plasma-desktop-doc \
-  plasma-drkonqi \
-  plasma-nm \
-  plasma-nm-l2tp \
-  plasma-nm-openconnect \
-  plasma-nm-openswan \
-  plasma-nm-openvpn \
-  plasma-nm-pptp \
-  plasma-nm-vpnc \
-  plasma-pa \
-  plasma-user-manager \
-  plasma-workspace \
-  plasma-workspace-geolocation \
-  polkit-kde \
-  qt5-qtbase-gui \
-  qt5-qtdeclarative \
-  sddm \
-  sddm-breeze \
-  sddm-kcm \
-  setroubleshoot \
-  sni-qt \
-  xorg-x11-drv-libinput
+echo "Installing the needed packages."
+sudo dnf install -y --setopt=install_weak_deps=False\
+@"Hardware Support" @Fonts\
+bluedevil \
+breeze-gtk \
+breeze-icon-theme \
+colord-kde \
+dolphin \
+glibc-all-langpacks \
+gnome-keyring-pam \
+kde-gtk-config \
+kde-partitionmanager \
+kde-style-breeze \
+kdegraphics-thumbnailers \
+kdeplasma-addons \
+kdialog \
+kdnssd \
+kf5-baloo-file \
+kf5-kipi-plugins \
+kf5-kwayland \
+khotkeys \
+kmenuedit \
+konsole5 \
+kscreen \
+kscreenlocker \
+ksshaskpass \
+kinfocenter \
+kwalletmanager5 \
+kwin-wayland kwrite \
+libinput \
+libwayland-* \
+mesa-dri-drivers mesa-va-drivers \
+NetworkManager-config-connectivity-fedora \
+pam-kwallet \
+phonon-qt5-backend-gstreamer \
+pinentry-qt \
+plasma-breeze \
+plasma-desktop \
+plasma-desktop-doc \
+plasma-nm \
+plasma-pa \
+plasma-systemmonitor \
+plasma-workspace-wallpapers \
+plasma-workspace-wayland \
+polkit-kde \
+qt5-qtbase-gui \
+qt5-qtdeclarative \
+qt6-qtwayland \
+sddm-breeze \
+sddm-kcm \
+sddm-wayland-plasma \
+sni-qt \
+vulkan \
+wayland-utils \
+xorg-x11-server-Xwayland \
+xwaylandvideobridge
+
+# disable file indexer (may slow down system otherwise)
+while true; do
+    read -p "Do you want to disable the baloo file indexer (sometimes needs many resources)? (y/n) " yn
+    case $yn in
+        [Yy]* ) balooctl disable && balooctl purge && break;;
+        [Nn]* ) echo "skipped." && break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# enable SDDM
+echo "Enabling the graphical login."
+sudo systemctl enable --now sddm && \
+sudo systemctl set-default graphical.target
